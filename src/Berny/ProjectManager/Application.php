7427827be080b0cef9a7f56abb9d1d082cbb3ed7
Berny Cantos <be@rny.cc>
2013-04-30 13:09:24 +0200

    Extract configuration to default methods

--- src/Berny/ProjectManager/Application.php
+++ src/Berny/ProjectManager/Application.php
@@ -13,24 +13,40 @@ namespace Berny\ProjectManager;
 
 use Berny\ProjectManager\Manager\ProjectManager;
 use Symfony\Component\Console\Application as BaseApplication;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 class Application extends BaseApplication
 {
     const NAME    = 'Project Manager Application';
     const VERSION = '0.1';
 
+    private $projectManager;
+
     public function __construct(ProjectManager $projectManager)
     {
+        $this->projectManager = $projectManager;
         parent::__construct(static::NAME, static::VERSION);
+    }
 
-        $this->getHelperSet()->set(new Command\Helper\DialogHelper());
+    protected function getDefaultCommands()
+    {
+        return array_merge(
+            parent::getDefaultCommands(),
+            array(
+              new Command\AddProjectCommand($this->projectManager),
+              new Command\RemoveProjectCommand($this->projectManager),
+              new Command\EnableProjectCommand($this->projectManager),
+              new Command\DisableProjectCommand($this->projectManager),
+              new Command\ListProjectCommand($this->projectManager),
+            )
+        );
+    }
 
-        $this->addCommands(array(
-            new Command\AddProjectCommand($projectManager),
-            new Command\RemoveProjectCommand($projectManager),
-            new Command\EnableProjectCommand($projectManager),
-            new Command\DisableProjectCommand($projectManager),
-            new Command\ListProjectCommand($projectManager),
-        ));
+    protected function getDefaultHelperSet()
+    {
+        $helperSet = parent::getDefaultHelperSet();
+        $helperSet->set(new Command\Helper\DialogHelper());
+        return $helperSet;
     }
 }
